6aa5f1f71c5d71bfdbd00021f453d7ef50cd8bbf
Anna <anna.brakowska91@gmail.com>
2018-12-26 10:16:55 -0500

    added singly linked list

--- singlyLinkedLinkList.js
+++ singlyLinkedLinkList.js
@@ -7,9 +7,22 @@ class Node {
   }
 }
 
-class linkedList {
+class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length += 1;
+    return this;
   }
 }
