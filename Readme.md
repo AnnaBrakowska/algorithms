2db7d45ca0e6575c0afb0272d433cf2f49c176c4
Anna <anna.brakowska91@gmail.com>
2018-12-26 09:57:12 -0500

    added readme with theory

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Linked List
+
+- do not have indexes
+- random access not allowed
+- good for insertion and deletion
+
+# Binary Search Tree
+
+# Bubble Sort
+
+# Insertion Sort
+
+# Merge Sort
---
+++ singlyLinkedLinkList.js
@@ -0,0 +1,15 @@
+//SINGLY LINKED LINK LIST
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
